2398e859626f5b32b9f0030cdf9d48a6c8cd069a
huynguyen <huy24005137@gmail.com>
2022-05-31 14:51:20 +0700

    i love you

---
+++ Handle.php
@@ -0,0 +1,36 @@
+<?php
+    class Handle{
+            public function index()
+            {
+                $email= $_POST['email'];
+                $password=$_POST['password'];
+                //Kiểm tra rỗng
+                if($email==''||$password=='')
+                {
+                    echo "Bạn chưa nhập email hoặc mật khẩu";
+                }
+                else
+                {   
+                    //Kiểm tra có phải định dạng gmail không
+                    $pattern="/[\S]+@gmail.com/";
+                    if(preg_match($pattern,$email)==true)
+                    {
+                        echo "Email: ".$email."<br></br>";
+                        echo "Mật khẩu:".$password;
+                       
+                    }
+                    else
+                    {
+                        echo "Bạn nhập chưa đúng định dạng email";
+                    }
+                    
+
+                }
+            }
+
+
+    }
+    $Handle=new Handle();
+    $Handle->index();
+
+?>
